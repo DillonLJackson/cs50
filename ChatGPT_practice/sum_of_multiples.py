c05127dbe6cea5f45b94fa0649b431b61f4fde13
Dillon Jackson <dillon.jackson26304@gmail.com>
2024-12-23 15:16:26 -0600

    yup

---
+++ ChatGPT_practice/sum_of_multiples.py
@@ -0,0 +1,8 @@
+# Write a program that calculates the sum of all multiples of 3 or 5 below 100. Use a loop to achieve this.
+def main():
+
+    def monster(energy):
+        while energy > 100:
+            sum(3 ** 2) 
+
+main()
