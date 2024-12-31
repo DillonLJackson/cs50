26c8e073be061dc78b4676383fb501c89b4dd257
DillonLJackson <dillon.jackson26304@gmail.com>
2024-12-31 14:54:15 -0600

    finished

--- ChatGPT_practice/tempconvert.py
+++ ChatGPT_practice/tempconvert.py
@@ -1,11 +1,21 @@
 # write a program that converts between celsius and fahrenheit
 
 def main():
+    # converting from celsius to fahrenheit. 
     c = int(input("Please enter a degree in celsius: "))
-    convert(c)
-    print(c)
+    conversion = (c * 1.8) + 32
+    
+    print(f"{c} degrees in Celcius is equal to {conversion} degrees in fahrenheit. ")
+    
 
-    def convert(fahrenheit):
-        fahrenheit = (convert * 9/5 + 32)
+    # converting from fahrenheit to celsius
+    f = int(input("Please enter a degree in Fahrenheit: "))
+    conversionf = (f - 32) * 5/9
+
+    print(f"{f} degrees in Fahrenheit is equal to {conversionf} degrees in Celsius. ")
+
+    
+
+
 
 main()
