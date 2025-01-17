a5f9bbbb04eb4b7b4afc4cc0028507da8cec59d7
Dillon Jackson <dillon.jackson26304@gmail.com>
2025-01-16 18:55:53 -0600

    finsished plates

--- problem_set2/plates.py
+++ problem_set2/plates.py
@@ -4,13 +4,32 @@ def main():
         print("Valid")
     else:
         print("Invalid")
-
-
+        
 def is_valid(s):
-        
-        
-       
 
+    #check that user input is within 2-6 characters
+    if len(s) > 1 and len(s) < 7:
+        # turn user input into a list
+        plate_list = list(s)
+        # checking that the first and second chr of user input is str
+        if plate_list[0].isalpha() and plate_list[1].isalpha():
+                # making sure there are no digits in the middle of user input
+                first_digit = False
+
+                for plate in plate_list:
+                    if plate.isdigit():
+                         if not first_digit and plate == '0':
+                              return False
+                         
+                         first_digit = True
+                    elif first_digit and not plate.isalpha():
+                         return False
+                return True
+        return False
+
+                
+             
+                
     
       
 
