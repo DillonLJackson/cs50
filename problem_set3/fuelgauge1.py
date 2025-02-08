d3828aa4c8c4ca425d5dbd5ffdfb241271475913
DillonLJackson <dillon.jackson26304@gmail.com>
2025-02-08 11:54:27 -0600

    l

---
+++ problem_set3/fuelgauge1.py
@@ -0,0 +1,19 @@
+def main():
+
+    print(valve)
+
+
+    def valve():
+    x = input("Fraction: ")
+    x, y = fuel.strip().split("/")
+
+    # give user input int value
+    num1 = int(x)
+    num2 = int(y)
+
+    # take the int value from user and convert them into fraction
+    valve = (num1 / num2) * 100
+    return valve
+
+
+main()
