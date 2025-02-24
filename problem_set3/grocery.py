0a207d896923caba68774d6084707140e87da33e
DillonLJackson <130684463+DillonLJackson@users.noreply.github.com>
2025-02-24 16:33:17 +0000

    i cannot

--- problem_set3/grocery.py
+++ problem_set3/grocery.py
@@ -2,14 +2,14 @@ def main():
 
     my_list = []
     while True:
-            try:
-                grocery = input("").title()
-                my_list.append(grocery)
-                my_list.sort()
-
-
-            except EOFError:
-                print(my_list.count())
-                exit()
+      
+        try:
+            grocery = input("").title()
+            my_list.append(grocery)
+            my_list.sort()
+               
+        except EOFError:
+            print(my_list.count())
+            exit()
 
 main()
